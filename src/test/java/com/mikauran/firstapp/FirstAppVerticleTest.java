b136311476d806e4e68f8f0db5930b15b600edb6
Miguel Lopez <mikauran@hotmail.com>
2020-07-04 20:22:22 -0500

    Vertex  First App using Gradle
    Builds with gradle instead maven

---
+++ src/test/java/com/mikauran/firstapp/FirstAppVerticleTest.java
@@ -0,0 +1,22 @@
+package com.mikauran.firstapp;
+import com.mikauran.FirstAppVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public class FirstAppVerticleTest {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new FirstAppVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    testContext.completeNow();
+  }
+}
